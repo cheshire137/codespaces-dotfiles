373dcca3abd08cfdf492a4367248326d8eef02dc
Sarah Vessels <cheshire137@gmail.com>
2025-08-20 10:40:01 -0500

    Move workspace setup into install.sh
    Not seeing any output to indicate codespaces-post-attach was being run, but install.sh does get run. Also don't try to use zsh now, stick with bash for simplicity.

--- install.sh
+++ install.sh
@@ -9,3 +9,18 @@ if [ -f "/workspaces/.codespaces/.persistedshare/dotfiles/.bashrc" ]; then
   # Leave what's in place there but append customizations
   echo "source '/workspaces/.codespaces/.persistedshare/dotfiles/.bashrc'" >> ~/.bashrc
 fi
+
+# Unfortunately, Codespaces doesn't have a way to open a default .code-workspace file,
+# see https://github.com/microsoft/vscode-remote-release/issues/3665
+file_path="/workspaces/.codespaces/.persistedshare/.workspace-opened"
+dotfiles_dir="/workspaces/.codespaces/.persistedshare/dotfiles"
+
+if [ ! -f "$file_path" ]; then
+  if [ -d "/workspaces/github" ] && [ -d "/workspaces/github-ui" ] && [ -d "$dotfiles_dir" ]; then
+    touch "$file_path"
+    echo "==> First run, loading workspace..."
+    code --reuse-window $dotfiles_dir/with-github-ui.code-workspace
+  fi
+elif [ -f "$file_path" ]; then
+  echo "==> $file_path exists, workspace already opened."
+fi
