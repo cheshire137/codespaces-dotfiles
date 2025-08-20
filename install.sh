55f665fcb4f85f2ab6d0d190ed356da38fc6156d
Sarah Vessels <cheshire137@gmail.com>
2025-08-20 11:15:45 -0500

    Bring over gitconfig and bash_profile to home

--- install.sh
+++ install.sh
@@ -6,3 +6,13 @@ if [ -f "/workspaces/.codespaces/.persistedshare/dotfiles/.bashrc" ]; then
   # Leave what's in place there but append customizations
   echo "source '/workspaces/.codespaces/.persistedshare/dotfiles/.bashrc'" >> ~/.bashrc
 fi
+
+if [ -f "/workspaces/.codespaces/.persistedshare/dotfiles/.gitconfig" ]; then
+  # Overwrite git config with my own
+  cp /workspaces/.codespaces/.persistedshare/dotfiles/.gitconfig ~/.gitconfig
+fi
+
+if [ -f "/workspaces/.codespaces/.persistedshare/dotfiles/.bash_profile" ]; then
+  # Overwrite bash profile with my own
+  cp /workspaces/.codespaces/.persistedshare/dotfiles/.bash_profile ~/.bash_profile
+fi
